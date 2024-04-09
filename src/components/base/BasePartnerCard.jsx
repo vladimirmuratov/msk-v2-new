3f00455c48a11e607c15677add8a1a486e80eb94
vladimir <vladimirmuratov@mail.ru>
2024-04-09 15:24:54 +0300

    update partners

--- src/components/base/BasePartnerCard.jsx
+++ src/components/base/BasePartnerCard.jsx
@@ -1,6 +1,6 @@
 import {Box, Typography} from '@mui/material'
 
-export const BasePartnerCard = ({logo, label}) => {
+export const BasePartnerCard = ({logo, label, address, license}) => {
     return(
         <Box sx={{
             border: '1px solid var(--green)',
@@ -8,7 +8,9 @@ export const BasePartnerCard = ({logo, label}) => {
             padding: '10px',
         }}>
             <img src={logo} alt="logo" style={{width: '100px'}}/>
-            <Typography sx={{fontSize: {xs: '12px', sm: '14px', md: '16px'}}}>{label}</Typography>
+            <Typography sx={{fontSize: {xs: '12px', sm: '14px', md: '16px'}, lineHeight: 1.3, marginBottom: '3px'}}>{label}</Typography>
+            <Typography sx={{fontSize: {xs: '10px', sm: '10px', md: '10px'}, color: 'var(--gray)'}}>{address}</Typography>
+            <Typography sx={{fontSize: {xs: '10px', sm: '10px', md: '10px'}, color: 'var(--gray)'}}>{license}</Typography>
         </Box>
     )
 }
