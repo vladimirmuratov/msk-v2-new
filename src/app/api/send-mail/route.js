937db9cdad35514cab35e5c4e3c8b7ea9d4ec9cd
vladimir <vladimirmuratov@mail.ru>
2024-04-13 21:02:51 +0300

    update route

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -3,18 +3,19 @@ import {NextResponse} from 'next/server'
 
 
 export async function POST(request) {
-    const res = await request.json()
+    const req = await request.json()
     let error = false
 
     const message = {
         from: process.env.ADDRESS_FROM,
         to: [process.env.ADDRESS_TO_1, process.env.ADDRESS_TO_2, process.env.ADDRESS_TO_3, process.env.ADDRESS_TO_4],
         subject: `Поступила заявка на звонок: ${new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'})}`,
-        html: ` <p>Имя: ${res?.person}</p>
-                <p>Дата рождения: ${res?.dateBirth ?? 'Нет'}</p>
-                <p>Телефон: ${res?.phone}</p>
-                <p>Email: ${res?.email ?? 'Нет'}</p>
-                <p>Информация: ${res?.info ?? 'Нет'}</p>`,
+        html: ` <p>Имя: ${req?.person}</p>
+                <p>Дата рождения: ${req?.dateBirth ?? 'Нет'}</p>
+                <p>Телефон: ${req?.phone}</p>
+                <p>Email: ${req?.email ?? 'Нет'}</p>
+                <p>Информация: ${req?.info ?? 'Нет'}</p>
+                <p>Сообщение пришло с сайта: msk-emergency.ru</p>`,
     }
 
     const transporter = nodemailer.createTransport({
@@ -29,6 +30,23 @@ export async function POST(request) {
         await transporter.sendMail(message)
     } catch (err) {
         error = true
+    }
+
+    //----------------------------------------
+
+    if(req.email){
+        const message2 = {
+            from: process.env.ADDRESS_FROM,
+            to: [req.email],
+            subject: `Вы оставили заявку на звонок на сайте msk-ambulance.ru ${new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Moscow'})}`,
+            html: ` <p>Имя: ${req?.person}</p>
+                    <p>Дата рождения: ${req?.dateBirth ?? 'Нет'}</p>
+                    <p>Телефон: ${req?.phone}</p>
+                    <p>Email: ${req?.email ?? 'Нет'}</p>
+                    <p>Информация: ${req?.info ?? 'Нет'}</p>`,
+        }
+
+        transporter.sendMail(message2)
     }
 
     return error
