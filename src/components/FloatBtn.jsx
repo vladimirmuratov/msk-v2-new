f8677e192e336f72d90c6f7504e8f9d2dbfad8fc
vladimir <vladimirmuratov@mail.ru>
2025-03-18 19:23:44 +0300

    update float btn

--- src/components/FloatBtn.jsx
+++ src/components/FloatBtn.jsx
@@ -1,36 +1,44 @@
 import { Fab, Link } from '@mui/material';
-import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled'
+import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
 import { phoneNumber } from '@/config';
 
-export const FloatBtn = ({handleOpen}) => {
+export const FloatBtn = ({ handleOpen, isMobile }) => {
     return (
-        /*<Fab
-            onClick={handleOpen}
-            color="error"
-            sx={{
-                position: 'fixed',
-                bottom: {xs: '10px', sm: '30px', md: '50px'},
-                right: {xs: '10px', sm: '30px', md: '50px'}
-            }}>
-            <PhoneEnabledIcon/>
-            <span className="pulse-button__rings"></span>
-            <span className="pulse-button__rings"></span>
-            <span className="pulse-button__rings"></span>
-        </Fab>*/
-
-        <Link href={`tel:${phoneNumber}`}>
-            <Fab
-                color="error"
-                sx={{
-                    position: 'fixed',
-                    bottom: { xs: '10px', sm: '30px', md: '50px' },
-                    right: { xs: '10px', sm: '30px', md: '50px' }
-                }}>
-                <PhoneEnabledIcon />
-                <span className="pulse-button__rings"></span>
-                <span className="pulse-button__rings"></span>
-                <span className="pulse-button__rings"></span>
-            </Fab>
-        </Link>
-    )
-}
+        <>
+            {typeof isMobile === 'boolean' &&
+            isMobile
+                ? (
+                    <Link href={`tel:${phoneNumber}`}>
+                        <Fab
+                            color="error"
+                            sx={{
+                                position: 'fixed',
+                                bottom: { xs: '10px', sm: '30px', md: '50px' },
+                                right: { xs: '10px', sm: '30px', md: '50px' }
+                            }}>
+                            <PhoneEnabledIcon />
+                            <span className="pulse-button__rings"></span>
+                            <span className="pulse-button__rings"></span>
+                            <span className="pulse-button__rings"></span>
+                        </Fab>
+                    </Link>
+                )
+                : (
+                    <Fab
+                        onClick={handleOpen}
+                        color="error"
+                        sx={{
+                            position: 'fixed',
+                            bottom: { xs: '10px', sm: '30px', md: '50px' },
+                            right: { xs: '10px', sm: '30px', md: '50px' }
+                        }}>
+                        <PhoneEnabledIcon />
+                        <span className="pulse-button__rings"></span>
+                        <span className="pulse-button__rings"></span>
+                        <span className="pulse-button__rings"></span>
+                    </Fab>
+                )
+            }
+        </>
+    );
+};
--- src/components/FormAction.jsx
+++ src/components/FormAction.jsx
@@ -1,63 +1,74 @@
-'use client'
+'use client';
 
-import {Box, DialogContentText} from '@mui/material'
-import {FloatBtn} from '@/components/FloatBtn'
-import {useEffect, useState} from 'react'
-import {MessageForm} from '@/components/MessageForm'
-import {timeOut} from '@/config'
-import {BaseModal} from '@/components/base/BaseModal'
+import { Box, DialogContentText } from '@mui/material';
+import { FloatBtn } from '@/components/FloatBtn';
+import { useEffect, useState } from 'react';
+import { MessageForm } from '@/components/MessageForm';
+import { timeOut } from '@/config';
+import { BaseModal } from '@/components/base/BaseModal';
 
 export const FormAction = () => {
-    const [open, setOpen] = useState(false)
-    const [sendForm, setSendForm] = useState(false)
-    const [isError, setError] = useState(false)
+    const [open, setOpen] = useState(false);
+    const [sendForm, setSendForm] = useState(false);
+    const [isError, setError] = useState(false);
+    const [isMobile, setMobile] = useState(undefined);
 
     useEffect(() => {
-        let timeout
+        const os = navigator.userAgentData.platform;
+
+        if (os === 'Android' || os === 'iOS') {
+            setMobile(true);
+        } else {
+            setMobile(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        let timeout;
         if (sendForm) {
             timeout = setTimeout(() => {
-                handleCloseSuccessMessage()
-            }, timeOut)
+                handleCloseSuccessMessage();
+            }, timeOut);
         }
         return () => {
-            clearTimeout(timeout)
-        }
-    }, [sendForm])
+            clearTimeout(timeout);
+        };
+    }, [sendForm]);
 
     useEffect(() => {
-        let timeout
+        let timeout;
         if (isError) {
             timeout = setTimeout(() => {
-                handleCloseFailedMessage()
-            }, timeOut)
+                handleCloseFailedMessage();
+            }, timeOut);
         }
         return () => {
-            clearTimeout(timeout)
-        }
-    }, [isError])
+            clearTimeout(timeout);
+        };
+    }, [isError]);
 
     const handleClickOpen = () => {
-        setOpen(true)
-    }
+        setOpen(true);
+    };
 
     const handleClose = () => {
-        setOpen(false)
-    }
+        setOpen(false);
+    };
 
     const handleCloseSuccessMessage = () => {
-        setSendForm(false)
-    }
+        setSendForm(false);
+    };
 
     const handleCloseFailedMessage = () => {
-        setError(false)
-    }
+        setError(false);
+    };
 
     return (
         <Box>
-            <FloatBtn handleOpen={handleClickOpen}/>
+            <FloatBtn handleOpen={handleClickOpen} isMobile={isMobile} />
 
             <BaseModal open={open} handleClose={handleClose} title="Связаться с нами">
-                <MessageForm handleClose={handleClose} onSendForm={setSendForm} onError={setError}/>
+                <MessageForm handleClose={handleClose} onSendForm={setSendForm} onError={setError} />
             </BaseModal>
 
             {sendForm && (
@@ -70,7 +81,8 @@ export const FormAction = () => {
             )}
 
             {isError && (
-                <BaseModal title="Ошибка сервера!" open={isError} handleClose={handleCloseFailedMessage} color="var(--red)">
+                <BaseModal title="Ошибка сервера!" open={isError} handleClose={handleCloseFailedMessage}
+                           color="var(--red)">
                     <DialogContentText id="alert-dialog-description">
                         Попробуйте позже
                     </DialogContentText>
@@ -78,5 +90,5 @@ export const FormAction = () => {
             )}
 
         </Box>
-    )
-}
+    );
+};
