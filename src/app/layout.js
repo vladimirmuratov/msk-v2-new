d7121b4592420852a82cf0e2fe2f867d8b75ce2a
vladimir <vladimirmuratov@mail.ru>
2025-03-28 14:17:00 +0300

    add openGraph

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,32 +12,40 @@ import './globals.css';
 const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
-  title: 'Платная госпитализация в Москве. 8 (499) 719-81-00, 24/7',
-  description:
-    '10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Оперативно. Минимум документов. Финансовая прозрачность.',
-  keywords: 'клиническая больница управления делами президента, фгбу объединенная больница с поликлиникой, больница управления делами президента, госпитализация без полиса омс, фгбу клиническая больница',
+    metadataBase: new URL('https://www.msk-emergency.ru'),
+    title: 'Платная госпитализация в Москве. 8 (499) 719-81-00, 24/7',
+    description:
+        '10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Оперативно. Минимум документов. Финансовая прозрачность.',
+    keywords: 'клиническая больница управления делами президента, фгбу объединенная больница с поликлиникой, больница управления делами президента, госпитализация без полиса омс, фгбу клиническая больница',
+    openGraph: {
+        title: 'Платная госпитализация в Москве. 8 (499) 719-81-00, 24/7',
+        description: '10 лет опыта. 20 клиник-партнеров. Федеральные, ведомственные и частные клиники. Оперативно. Минимум документов. Финансовая прозрачность.',
+        images: ['/images/banner3.webp'],
+        url: 'https://www.msk-emergency.ru',
+        type: 'website',
+    }
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="yandex-verification" content="dd2e2dbaa184eaca" />
-      </head>
-      <body className={montserrat.className}>
+    return (
+        <html lang="en">
+        <head>
+            <meta name="yandex-verification" content="dd2e2dbaa184eaca" />
+        </head>
+        <body className={montserrat.className}>
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <Box className="container">
-              <Suspense>
-                <Navigation />
-                {children}
-                <Metrika />
-                <Footer />
-              </Suspense>
-            </Box>
-          </ThemeProvider>
+            <ThemeProvider theme={theme}>
+                <Box className="container">
+                    <Suspense>
+                        <Navigation />
+                        {children}
+                        <Metrika />
+                        <Footer />
+                    </Suspense>
+                </Box>
+            </ThemeProvider>
         </AppRouterCacheProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
