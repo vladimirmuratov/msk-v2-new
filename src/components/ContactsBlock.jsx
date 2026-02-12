87f1111ec78761462b24e361bff0bc6e8454bfb0
vladimir <vladimirmuratov@mail.ru>
2026-02-12 17:41:01 +0300

    update massages icons

--- src/components/ContactsBlock.jsx
+++ src/components/ContactsBlock.jsx
@@ -1,74 +1,86 @@
 import { Box, Link, Typography } from '@mui/material';
 import { email, phoneNumber, phoneNumber2 } from '@/config';
-// import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
-import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import CallIcon from '@mui/icons-material/Call';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import SocialBlock from '@/components/SocialBlock';
+// import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
 export const ContactsBlock = () => {
-    return (
-        <Box
-            component="section"
-            id="contact"
-            sx={{
-                scrollMarginTop: { xs: '120px', sm: '50px' },
-                paddingX: { xs: '10px', lg: 0 },
-                paddingY: { xs: '25px', sm: '50px' },
-                color: 'var(--black)'
-            }}
-        >
-            <Typography
-                variant="h2"
-                sx={{
-                    marginBottom: '20px',
-                    fontSize: { xs: '28px', sm: '34px' },
-                    fontWeight: 400,
-                    color: 'var(--red)'
-                }}>Контакты</Typography>
+  return (
+    <Box
+      component="section"
+      id="contact"
+      sx={{
+        scrollMarginTop: { xs: '120px', sm: '50px' },
+        paddingX: { xs: '10px', lg: 0 },
+        paddingY: { xs: '25px', sm: '50px' },
+        color: 'var(--black)',
+      }}
+    >
+      <Typography
+        variant="h2"
+        sx={{
+          marginBottom: '20px',
+          fontSize: { xs: '28px', sm: '34px' },
+          fontWeight: 400,
+          color: 'var(--red)',
+        }}
+      >
+        Контакты
+      </Typography>
 
-            <Box
-                sx={{ display: 'grid', gridTemplateColumns: { xs: '1fr', sm: '1fr 2fr' }, gap: { xs: '10px', sm: 0 } }}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-
-                    {/*<Box sx={{ display: 'flex', gap: '10px' }}>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { xs: '1fr', sm: '1fr 2fr' },
+          gap: { xs: '10px', sm: 0 },
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+          {/*<Box sx={{ display: 'flex', gap: '10px' }}>
                         <MapOutlinedIcon sx={{ color: 'var(--main-color)' }} />
                         <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>
                             г. Москва, Мичуринский пр-т д.6
                         </Typography>
                     </Box>*/}
 
-                    <Box sx={{ display: 'flex', alignItems: 'start', gap: '10px' }}>
-                        <LocalPhoneOutlinedIcon sx={{ color: 'var(--main-color)' }} />
-                        <Box sx={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
-                            <Link href={`tel:${phoneNumber}`}
-                                  sx={{ fontSize: { xs: '14px', sm: '16px' } }}>{phoneNumber}</Link>
-                            <Link href={`tel:${phoneNumber2}`}
-                                  sx={{ fontSize: { xs: '14px', sm: '16px' } }}>{phoneNumber2}</Link>
-                        </Box>
-                    </Box>
-                    <Box sx={{ display: 'flex', gap: '10px' }}>
-                        <AlternateEmailIcon sx={{ color: 'var(--main-color)' }} />
-                        <Link href={`mailto:${email}`} sx={{ fontSize: { xs: '14px', sm: '16px' } }}>{email}</Link>
-                    </Box>
-                    <Box sx={{ paddingLeft: '34px' }}>
-                        <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>ООО "МСК"</Typography>
-                        <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>ИНН: 9725116009</Typography>
-                        <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>КПП: 772501001</Typography>
-                        <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>ОГРН: 1237700147343</Typography>
-                        <Box sx={{ marginTop: '15px' }}>
-                            <SocialBlock className="banner-social-mobile" isIconColored={true} />
-                        </Box>
-                    </Box>
-
-
-                </Box>
-                <Box sx={{visibility: 'hidden'}}>
-                    <iframe
-                        className="map"
-                        src="https://yandex.ru/map-widget/v1/?um=constructor%3A604ee8345ca097d664aa1c7764529b62cb0469a88360ead0ce5d5df219788165&amp;source=constructor"
-                        frameBorder="0"></iframe>
-                </Box>
+          <Box sx={{ display: 'flex', alignItems: 'start', gap: '10px' }}>
+            <CallIcon sx={{ color: 'var(--main-color)' }} />
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+              <Link href={`tel:${phoneNumber}`} sx={{ fontSize: { xs: '14px', sm: '16px' } }}>
+                {phoneNumber}
+              </Link>
+              <Link href={`tel:${phoneNumber2}`} sx={{ fontSize: { xs: '14px', sm: '16px' } }}>
+                {phoneNumber2}
+              </Link>
             </Box>
+          </Box>
+          <Box sx={{ display: 'flex', gap: '10px' }}>
+            <AlternateEmailIcon sx={{ color: 'var(--main-color)' }} />
+            <Link href={`mailto:${email}`} sx={{ fontSize: { xs: '14px', sm: '16px' } }}>
+              {email}
+            </Link>
+          </Box>
+          <Box sx={{ paddingLeft: '34px' }}>
+            <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>ООО "МСК"</Typography>
+            <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>ИНН: 9725116009</Typography>
+            <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>КПП: 772501001</Typography>
+            <Typography sx={{ fontSize: { xs: '14px', sm: '16px' } }}>
+              ОГРН: 1237700147343
+            </Typography>
+            <Box sx={{ marginTop: '15px' }}>
+              <SocialBlock className="banner-social-mobile" isIconColored={true} />
+            </Box>
+          </Box>
         </Box>
-    );
+        <Box sx={{ visibility: 'hidden' }}>
+          <iframe
+            className="map"
+            src="https://yandex.ru/map-widget/v1/?um=constructor%3A604ee8345ca097d664aa1c7764529b62cb0469a88360ead0ce5d5df219788165&amp;source=constructor"
+            frameBorder="0"
+          ></iframe>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
--- src/components/SocialBlock.jsx
+++ src/components/SocialBlock.jsx
@@ -1,83 +1,88 @@
-import React from 'react'
-import {Box, Link} from '@mui/material'
-import Image from 'next/image'
-import { email, phoneTelegram, phoneViber, phoneWhatsApp } from '@/config';
+import React from 'react';
+import { Box, Link } from '@mui/material';
+import Image from 'next/image';
+import { email, maxLink, telegramLink } from '@/config';
+import { MaxIcon } from '@/components/icons/MaxIcon';
 
-const SocialBlock = ({className = '', isIconColored = false}) => {
-    return (
-        <Box
-            className={className}
-            sx={{
-                // width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-                gap: '15px',
-            }}
-        >
-            <Link
-                target="_blank"
-                href={`https://wa.me/${phoneWhatsApp}`}
-                sx={{
-                    transition: 'all 0.3s',
-                    '&:hover': {
-                        transform: 'scale(1.1)',
-                    }
-                }}
-            >
-                <Image
-                    // src="/images/social/whatsapp.svg"
-                    src={isIconColored ? '/images/social/whatsapp-blue.svg' : '/images/social/whatsapp-white.svg'}
-                    alt="icon"
-                    width={30}
-                    height={30}
-                />
-            </Link>
-            <Link
-                target="_blank"
-                href={`https://t.me/${phoneTelegram}`}
-                sx={{
-                    transition: 'all 0.3s',
-                    '&:hover': {
-                        transform: 'scale(1.1)',
-                    }
-                }}
-            >
-                <Image
-                    // src="/images/social/telegram.svg"
-                    src={isIconColored ? '/images/social/telegram-blue.svg' : '/images/social/telegram-white.svg'}
-                    alt="icon"
-                    width={30}
-                    height={30}
-                />
-            </Link>
-            {/*<Link href={`viber://add?number=${phoneViber}`}>
-                <Image
-                    src="/images/social/viber.svg"
-                    alt="icon"
-                    width={30}
-                    height={30}
-                />
-            </Link>*/}
-            <Link
-                href={`mailto:${email}`}
-                sx={{
-                    transition: 'all 0.3s',
-                    '&:hover': {
-                        transform: 'scale(1.1)',
-                    }
-                }}
-            >
-                <Image
-                    // src="/images/social/email.svg"
-                    src={isIconColored ? '/images/social/email-blue.svg' : '/images/social/email-white.svg'}
-                    alt="icon"
-                    width={30}
-                    height={30}
-                />
-            </Link>
-        </Box>
-    )
-}
+const SocialBlock = ({ className = '', isIconColored = false }) => {
+  return (
+    <Box
+      sx={{
+        // width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: '15px',
+      }}
+      className={className}
+    >
+      {/*<Link
+        target="_blank"
+        href={`https://wa.me/${phoneWhatsApp}`}
+        sx={{
+          transition: 'all 0.3s',
+          '&:hover': {
+            transform: 'scale(1.1)',
+          },
+        }}
+      >
+        <Image
+          // src="/images/social/whatsapp.svg"
+          src={isIconColored ? '/images/social/whatsapp-blue.svg' : '/images/social/whatsapp-white.svg'}
+          alt="icon"
+          width={30}
+          height={30}
+        />
+      </Link>*/}
+      <Link
+        target="_blank"
+        href={maxLink}
+        sx={{
+          transition: 'all 0.3s',
+          '&:hover': {
+            transform: 'scale(1.1)',
+          },
+        }}
+      >
+        {isIconColored ? <MaxIcon color="var(--main-color)" /> : <MaxIcon color="var(--white)" />}
+      </Link>
+      <Link
+        target="_blank"
+        href={telegramLink}
+        sx={{
+          transition: 'all 0.3s',
+          '&:hover': {
+            transform: 'scale(1.1)',
+          },
+        }}
+      >
+        <Image
+          src={
+            isIconColored ? '/images/social/telegram-blue.svg' : '/images/social/telegram-white.svg'
+          }
+          alt="icon"
+          width={30}
+          height={30}
+        />
+      </Link>
+      <Link
+        href={`mailto:${email}`}
+        sx={{
+          transition: 'all 0.3s',
+          '&:hover': {
+            transform: 'scale(1.1)',
+          },
+        }}
+      >
+        <Image
+          src={isIconColored ? '/images/social/email-blue.svg' : '/images/social/email-white.svg'}
+          alt="icon"
+          width={30}
+          height={30}
+        />
+      </Link>
+    </Box>
+  );
+};
 
-export default React.memo(SocialBlock)
+export default React.memo(SocialBlock);
